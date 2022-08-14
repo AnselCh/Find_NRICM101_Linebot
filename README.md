e991c569ea5872cce42a83dbc499d80fc2aeee30
Ching Yi, Chan <qrtt1@infuseai.io>
2022-08-14 11:30:37 +0800

    Update readme
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- README.md
+++ README.md
@@ -1,3 +1,54 @@
 # Find_NRICM101
+
 提供更快速找住家附近有提供公費的清冠一號診所
 Easy to find where has free 'NRICM101'.
+
+## Usage
+
+Create a venv and install dependencies
+
+```
+python -mvenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+Run the script and enter partial of your address (eg. `仁愛路`)
+
+```
+$ python help_to_find.py
+請輸入想查詢的區域或路名:仁愛路
+診所:順泰中醫診所
+電話:04-7294699
+地址:彰化市仁愛路110號
+剩餘清冠一號數量(依廠牌顯示)
+順天堂:0
+莊松榮:0
+立康:0
+勝昌:0
+勸奉堂:3
+華佗:0
+天一:36
+漢聖:0
+天明:0
+科達:0
+富田:0
+--------------------
+診所:永和健生堂中醫診所
+電話:02-86608198
+地址:新北市永和區仁愛路218號
+剩餘清冠一號數量(依廠牌顯示)
+順天堂:0
+莊松榮:0
+立康:0
+勝昌:0
+勸奉堂:5
+華佗:0
+天一:10
+漢聖:0
+天明:100
+科達:0
+富田:5
+...
+```
+
--- help_to_find.py
+++ help_to_find.py
@@ -9,56 +9,50 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-headers = {'user-agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
-url="https://docs.google.com/spreadsheets/d/e/2PACX-1vQjf_HNeEZKM-XJX-q5v4cfNrB3kcv4gOT8kFbV9rurfoX_H5Qv9112Pv0PgYNFSzbReyNlQkLrJib3/pubhtml?utm_source=google&utm_medium=cpc&utm_campaign=%E6%B8%85%E5%86%A0%E4%B8%80%E8%99%9F%E5%8B%95%E6%85%8B%E8%A1%A8#"
-html = requests.get(url,headers=headers)
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'}
+url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQjf_HNeEZKM-XJX-q5v4cfNrB3kcv4gOT8kFbV9rurfoX_H5Qv9112Pv0PgYNFSzbReyNlQkLrJib3/pubhtml?utm_source=google&utm_medium=cpc&utm_campaign=%E6%B8%85%E5%86%A0%E4%B8%80%E8%99%9F%E5%8B%95%E6%85%8B%E8%A1%A8#"
+html = requests.get(url, headers=headers)
 if html.status_code == requests.codes.ok:
-    get= BeautifulSoup(html.text, "lxml")
-#抓出全台提供公費清冠一號的中醫診所 
+    get = BeautifulSoup(html.text, "lxml")
+# 抓出全台提供公費清冠一號的中醫診所
 
-data1=get.find('div',class_="ritz grid-container").tbody.find_all('tr')
-#透過表格中輸入關鍵字列出想找的區域內的中醫
+data1 = get.find('div', class_="ritz grid-container").tbody.find_all('tr')
+# 透過表格中輸入關鍵字列出想找的區域內的中醫
 area = input(str("請輸入想查詢的區域或路名:"))
 alldata = []
-a=0
-
+a = 0
 
 while True:
-    
-    if re.match("[\u4e00-\u9fa5]+",area) == None:
+
+    if re.match("[\u4e00-\u9fa5]+", area) == None:
         print('請輸入正確的區域或路名')
         break
-        
-    
+
     for i in data1:
         alldata.append(i.find_all('td'))
-    #print(len(alldata))    
+    # print(len(alldata))
     for j in alldata:
-        
-        if area in j[21].get_text() :
+
+        if area in j[21].get_text():
             search = j
-            print('診所:%s\n電話:%s\n地址:%s' %(search[2].text,search[20].text,search[21].text))
+            print('診所:%s\n電話:%s\n地址:%s' % (search[2].text, search[20].text, search[21].text))
             print('剩餘清冠一號數量(依廠牌顯示)')
-            print('順天堂:%s'%search[3].text)
-            print('莊松榮:%s'%search[4].text)
-            print('立康:%s'%search[5].text)
-            print('勝昌:%s'%search[6].text)
-            print('勸奉堂:%s'%search[7].text)
-            print('華佗:%s'%search[8].text)
-            print('天一:%s'%search[9].text)
-            print('漢聖:%s'%search[10].text)
-            print('天明:%s'%search[11].text)
-            print('科達:%s'%search[12].text)
-            print('富田:%s'%search[13].text)
-            print('-'*20)
-            
+            print('順天堂:%s' % search[3].text)
+            print('莊松榮:%s' % search[4].text)
+            print('立康:%s' % search[5].text)
+            print('勝昌:%s' % search[6].text)
+            print('勸奉堂:%s' % search[7].text)
+            print('華佗:%s' % search[8].text)
+            print('天一:%s' % search[9].text)
+            print('漢聖:%s' % search[10].text)
+            print('天明:%s' % search[11].text)
+            print('科達:%s' % search[12].text)
+            print('富田:%s' % search[13].text)
+            print('-' * 20)
+
         else:
-            a +=1
+            a += 1
     break
     if a == 1558:
         print('請輸入正確的區域或路名')
-    
-
-
-
-
